afd080c4551ec087da141d9afb5077e225c77366
NoyatAvigail <avigaildvd@gmail.com>
2025-06-03 16:46:00 +0300

    contact work in postman

--- Server/app.js
+++ Server/app.js
@@ -4,9 +4,7 @@ import userRoutes from './routes/userRoutes.js';
 import genericRoutes from './routes/genericRoutes.js';
 
 const app = express();
-// app.use(express.json());
 app.use(express.json({ limit: '10mb' }));
-// app.use(cors());
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
