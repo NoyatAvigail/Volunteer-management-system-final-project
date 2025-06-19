c42897962cc3d2435199623febffd1ede1617ebf
user <user@DESKTOP-UL7S2GB>
2025-06-19 05:10:08 +0300

    email new

--- Server/services/emailsService.js
+++ Server/services/emailsService.js
@@ -1,18 +1,22 @@
 import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+const emailsService = {
+  transporter: nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  }),
 
-export default async function sendEditVerificationMail(to, code) {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    Subject: 'Verification code for profile editing',
-    html: `<p>Your verification code is: <strong>${code}</strong></p><p>The code is valid for 10 minutes.</p>`
-  });
-}
+  sendEditVerificationMail: async (to, code) => {
+    await emailsService.transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject: 'Verification code for profile editing',
+      html: `<p>Your verification code is: <strong>${code}</strong></p><p>The code is valid for 10 minutes.</p>`
+    });
+  }
+};
+
+export default emailsService;
