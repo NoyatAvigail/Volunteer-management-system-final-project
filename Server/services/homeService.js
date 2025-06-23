06d89e7a5aa6af3a8cac8a869e3d54149185a802
NoyatAvigail <avigaildvd@gmail.com>
2025-06-23 05:35:19 +0300

    home

--- Server/services/homeService.js
+++ Server/services/homeService.js
@@ -1,19 +1,5 @@
 import homeDal from '../dal/homeDal.js';
 
-// const homeService = {
-//     async gethome() {
-//         const [volunteerCount, totalHours] = await Promise.all([
-//             homeDal.countActiveVolunteers(),
-//             homeDal.sumVolunteerHours(),
-//         ]);
-//         return {
-//             volunteerCount,
-//             totalHours,
-//         };
-//     }
-// };
-
-// export default homeService;
 const homeService = {
     async gethome() {
         const [volunteerCount, totalHours, hospitalCount, departmentCount] = await Promise.all([
